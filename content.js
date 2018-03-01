738940e9deaee290eae5baa69077b624c5f696da
miviro <miviro0804@gmail.com>
2018-03-01 18:55:41 +0100

    Images

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-const REQUEST_URL = "http://oxygenrain.com/clickbait/timemarks.php?";
-
 // generic error handler
 function onError(error){
     console.log(error);
@@ -14,31 +12,6 @@ function GetVideoID(){
 var id = GetVideoID()[0];
 console.log("Working with: " + id);
 
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        var i = 0;
-        while(i < 10)
-        {
-            console.log("------------- Attempt " + i + "----------------");
-            i++;
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-                console.log("all ok, breaking");
-                callback(xmlHttp.responseText);
-                break;
-            } else {
-                console.log("ERROR: \n\tready state: "+ xmlHttp.readyState + ", status: " + xmlHttp.status)
-            }
-        }
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    console.log("sending");
-    xmlHttp.send(null);
-}
+browser.runtime.sendMessage({"url": e.target.href});
 
-console.log("asking for" + REQUEST_URL+id)
-httpGetAsync(REQUEST_URL+id, function(rp){
-    console.log("done");
-    document.getElementById("count").innerHTML = rp;
-});
+document.getElementById("count").innerHTML = rp;
