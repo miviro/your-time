4b5e660b2d905ae65d86e73ed44eab58a690dde1
miviro <miviro0804@gmail.com>
2018-03-14 00:24:32 +0100

    Added seconds to time, buggy Response to HTML

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
-const REQUEST_URL = "https://oxygenrain.com/yourtime/timemarks.php?";
+const SELECT_URL = "https://oxygenrain.com/yourtime/select.php?";
+const INSERT_URL = "https://oxygenrain.com/yourtime/insert.php?";
 
-function httpGetAsync(theUrl, callback)
-{
+function httpGetAsync(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange =
     function() {
@@ -12,19 +12,49 @@ function httpGetAsync(theUrl, callback)
     xmlHttp.send(null);
 }
 
+// transforms seconds to (days):(hours):minutes:seconds
+function SecondsToString(ss) {
+    if (ss<0) { return "negative time";}
+    var dd = Math.floor(ss / (3600*24));
+    ss  -= dd*3600*24;
+    var hh   = Math.floor(ss / 3600);
+    ss  -= hh*3600;
+    var mm = Math.floor(ss / 60);
+    ss  -= mm*60;
+    // These lines ensure you have two-digits
+    if (dd < 10) {dd = "0"+dd;}
+    if (hh < 10) {hh = "0"+hh;}
+    if (mm < 10) {mm = "0"+mm;}
+    if (ss < 10) {ss = "0"+ss;}
+    var time = dd+":"+hh+":"+mm+":"+ss;
+
+    // removes unnecessary "00:"s, ignoring the last two for formatting
+    return time.replace(/^(00\:){1,2}/gm, "");
+}
+
+// encodes the response as html
+function ResponseToHTML(response) {
+    var html = "<div id=\"your-time\">";
+    response.forEach(element => {
+        html += "<div class=\"submission\">";
+            html += "<h3 class=\"seconds\">" + SecondsToString(element["time"]) + "</h3>";
+            html += "<h3 class=\"comment\">" + element["comment"]               + "</h3>";
+            html += "<h3 class=\"votes\">"   + element["votes"]                 + "</h3>";
+        html += "</div>";
+    });
+    html += "</div>";
+}
+
 // gets id of youtube video from url
 // regex ignores "v=" to reuse on GET method
 var id = window.location.href.match(/v=[^&]*/);
 
-var htmltemplate = "";
-
-fetch('template.html')
-.then(response => response.text())
-.then(text => htmltemplate=text);
-
-document.getElementById("container").innerHTML += htmltemplate;
-
-httpGetAsync(REQUEST_URL + id, function(rp){
-
-    console.log(id + " done");
+httpGetAsync(SELECT_URL + id, function(rp) {
+    // if its not empty meaning there is data
+    if (rp!="[]") {
+        var response = JSON.parse(rp);
+        var html = ResponseToHTML(response);
+        console.log(html);
+        document.getElementById("info-contents").innerHTML += html;
+    }
 });
