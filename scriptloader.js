1712388efdafae21be9c4db5696f301cf1fd3e81
miviro <miviro0804@gmail.com>
2018-04-14 12:57:15 +0200

    Loads content.js to the webpage to use YT's API

---
+++ scriptloader.js
@@ -0,0 +1,14 @@
+var s = document.createElement("script");
+s.src = browser.extension.getURL("content.js");
+s.onload = function() {
+    this.remove();
+};
+
+// This meta element contains the stylesheets' internal url so that the injected script
+// can access it since it cannot use the browser's APIs
+var meta = document.createElement('meta');
+meta.name = "stylesheet-internal-url";
+// TODO: unify the stylesheet
+meta.content = browser.extension.getURL("stylesheet.css");
+(document.head || document.documentElement).appendChild(s);
+(document.head || document.documentElement).appendChild(meta);
